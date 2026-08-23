02281963fb08853342d24132b05916142ff4cccd
F-Papa <fpapa@fi.uba.ar>
2026-08-23 16:58:58 -0300

    docs(golang): improve interfaces documentation

--- golang/internal/middleware/middleware.go
+++ golang/internal/middleware/middleware.go
@@ -31,7 +31,8 @@ type Message struct {
 }
 
 type Producer interface {
-	// Se envía un mensaje a la cola
+	// Se bloquea y hasta publicar un mensaje a la cola o el contexto sea cancelado.
+	// Si ocurre un error interno que no puede resolverse devuelve ErrClosed.
 	Send(ctx context.Context, msg Message) error
 
 	// Se desconecta del MOM al que estaba conectado.
@@ -40,10 +41,11 @@ type Producer interface {
 }
 
 type Consumer interface {
-	// Se bloquea y consume mensajes de la queue o topic establecido
+	// Se bloquea y consume mensajes de la queue hasta que el contexto no sea cancelado u ocurra un error.
+	// Si ocurre un error interno que no puede resolverse devuelve ErrClosed.
 	Consume(ctx context.Context, msgs chan<- Message) error
 
-	// Deja de consumir
+	// Cancela el contexto del método Consume, desbloqueando el llamado
 	StopConsuming()
 
 	// Se desconecta del MOM al que estaba conectado.
@@ -52,22 +54,24 @@ type Consumer interface {
 }
 
 type Publisher interface {
-	// Se publica un mensaje al topic
+	// Se bloquea y hasta publicar un mensaje al topic o el contexto sea cancelado.
+	// Si ocurre un error interno que no puede resolverse devuelve ErrClosed.
 	Publish(ctx context.Context, msg Message) error
 
-	// Se desconecta del Mom al que etaba conectado.
+	// Se desconecta del MOM al que etaba conectado.
 	// Si ocurre un error interno que no puede resolverse devuelve ErrClosed.
 	Close() error
 }
 
 type Subscriber interface {
-	// Se bloquea y consume mensajes de la queue o topic establecido
+	// Se bloquea y consume mensajes del topic hasta que el contexto no sea cancelado u ocurra un error.
+	// Si ocurre un error interno que no puede resolverse devuelve ErrClosed.
 	Consume(ctx context.Context, msgs chan<- Message) error
 
-	// Deja de consumir
+	// Cancela el contexto del método Consume, desbloqueando el llamado
 	StopConsuming()
 
-	// Se desconecta del Mom al que etaba conectado.
+	// Se desconecta del MOM al que estaba conectado.
 	// Si ocurre un error interno que no puede resolverse devuelve ErrClosed.
 	Close() error
 }
